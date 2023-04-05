d1b73b561dcc1195ca5e3e5a399991809c99308c
AnoonaNasrin <anoonanasrinp24@gmail.com>
2023-04-05 12:39:04 +0530

    first commit

---
+++ server/models/bookform.js
@@ -0,0 +1,77 @@
+const mongoose = require('mongoose')
+
+const hotelbookSchema = mongoose.Schema(
+    {
+        fullname: {
+            type: String,
+
+        },
+        mobileno: {
+            type: String,
+        
+        },
+        content: {
+            type: String,
+
+        },
+        bookingurl: {
+            type: String,
+
+        },
+        availableroom: [
+            {
+                imgurl: {
+                    type: String,
+            
+                },
+                title2: {
+                    type: String,
+                
+                },
+                roomcapacity: 
+                    {
+                        max: {
+                            type: String,
+                        
+                        },
+                        min: {
+                            type: String,
+                    
+                        },
+
+                    },
+                
+                perRoom: {
+                    type: String,
+                   
+                },
+                adults: {
+                    type: String,
+                 
+                },
+                chlidren: {
+                    type: String,
+               
+                },
+                room: {
+                    type: String,
+                  
+                },
+                leftroom: {
+                    type: String,
+                    
+                },
+                perRoomPerWithBreakFast: {
+                    type: String,
+                  
+                }
+            }
+        ]
+
+    },
+)
+
+const HotelBook = mongoose.model('HotelBook', hotelbookSchema);
+
+
+module.exports = HotelBook;
